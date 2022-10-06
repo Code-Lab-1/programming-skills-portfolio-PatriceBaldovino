f5d20184cc65409e18f9817ad424c217e1430c7c
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-06 19:48:15 +0400

    Update Exercise_4.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
@@ -2,9 +2,10 @@
 
     #Write three strings in different variables and print the output as one string.
 #The Variables
-a = "He is half of my soul, as the poets say. "
-b = "He will be dead soon, and his honor is all that will remain. "
-c = "It is his child, his dearest self."
+a = '"He is half of my soul, as the poets say. '
+b = 'He will be dead soon, and his honor is all that will remain. '
+c = 'It is his child, his dearest self."'
+author = ' - Madeline Miller, The Song of Achilles'
 
 #Printing of the combined string
-print(a + b + c)
+print(a + b + c + author)
