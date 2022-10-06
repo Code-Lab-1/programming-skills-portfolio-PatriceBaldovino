0f4fafa10a4880b0384869056d9f131ec6da05ab
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-06 14:02:32 +0400

    Create Chapter_1_Answers.py

--- Chapter 1- Getting Started with Python/Exercises/Chapter_1_Answers.py
+++ Chapter 1- Getting Started with Python/Exercises/Chapter_1_Answers.py
@@ -35,3 +35,7 @@ b = "appreciate "
 c = "you <3"
 
 print (a + b + c)
+
+## Exercise 5: Compute area of Circle :ballot_box_with_check:
+
+ #Write a Python program which accepts the radius of a circle from the user and compute the area.
