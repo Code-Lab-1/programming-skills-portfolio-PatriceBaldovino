e487b0bdf3b0ab070b32c82493fb19070167e6e1
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-06 14:00:14 +0400

    Update Chapter_1.py

--- Chapter 1- Getting Started with Python/Exercises/Chapter_1.py
+++ Chapter 1- Getting Started with Python/Exercises/Chapter_1.py
@@ -3,16 +3,35 @@
 ##Write a Python program to print the following string in a specific format.
 
 #Twinkle, twinkle, little star,
-#	How I wonder what you are! 
-#		Up above the world so high,   		
-#		Like a diamond in the sky. 
+	#How I wonder what you are! 
+		#Up above the world so high,   		
+		#Like a diamond in the sky. 
 #Twinkle, twinkle, little star, 
-#	How I wonder what you are
+	#How I wonder what you are
 
-## Answers to Exercise 1:
+ # Answers to Exercise 1:
 print ("Twinkle, twinkle, little star,") 
 print ("	How I wonder what you are!")
 print ("		Up above the world so high,")
 print ("		Like a diamond in the sky.")
 print ("Twinkle, twinkle, little star,")
 print ("	How I wonder what you are")
+
+## Exercise 2: Print the Version of Python :ballot_box_with_check:
+
+ #Write a Python program to get the Python version you are using.
+ 
+
+## Exercise 3: Print date and Time :ballot_box_with_check:
+
+ #Write a Python program to display the current date and time.
+
+
+## Exercise 4: Strings Concatination :ballot_box_with_check:
+ #Write three strings in different variables and print the output as one string.
+
+a = "I "
+b = "appreciate "
+c = "you <3"
+
+print (a + b + c)
