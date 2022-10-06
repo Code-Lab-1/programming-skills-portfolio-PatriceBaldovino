0179d4032497eabef37afd9a3f4bd0ea15a7fbaf
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-06 13:50:52 +0400

    Update README.md
    yeh

---
+++ Chapter 1- Getting Started with Python/Exercises/Chapter_1.py
@@ -0,0 +1,18 @@
+## Exercise 1: Print Strings :ballot_box_with_check:
+
+##Write a Python program to print the following string in a specific format.
+
+#Twinkle, twinkle, little star,
+#	How I wonder what you are! 
+#		Up above the world so high,   		
+#		Like a diamond in the sky. 
+#Twinkle, twinkle, little star, 
+#	How I wonder what you are
+
+## Answers to Exercise 1:
+print ("Twinkle, twinkle, little star,") 
+print ("	How I wonder what you are!")
+print ("		Up above the world so high,")
+print ("		Like a diamond in the sky.")
+print ("Twinkle, twinkle, little star,")
+print ("	How I wonder what you are")
