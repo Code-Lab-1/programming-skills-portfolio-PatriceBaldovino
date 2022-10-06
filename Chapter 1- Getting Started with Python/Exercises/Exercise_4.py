bae205b748bd6734e77400505aa44512e25b5def
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-06 19:30:55 +0400

    Create Exercise_1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
@@ -1,9 +1,10 @@
 ## Exercise 4: Strings Concatination :ballot_box_with_check:
 
     #Write three strings in different variables and print the output as one string.
-
+#The Variables
 a = "He is half of my soul, as the poets say. "
 b = "He will be dead soon, and his honor is all that will remain. "
 c = "It is his child, his dearest self."
 
+#Printing of the combined string
 print(a + b + c)
---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_1.py
@@ -0,0 +1,8 @@
+## Exercise 1: Variables:ballot_box_with_check:
+
+    #Type out the code below and add the correct data types to complete the variables
+
+        #Assign a message to a variable, and print that message.
+        #Then change the value of the variable to a new message, and print the newmessage.
+
+
