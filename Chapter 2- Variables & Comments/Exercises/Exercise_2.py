a3d7bd2509ebae126ee1266f11a350d05c28b544
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-06 19:43:04 +0400

    Create Exercise_2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_2.py
@@ -0,0 +1,19 @@
+## Exercise 2: Variables :ballot_box_with_check:
+
+    #Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
+        #Your output should look something like the following, including the quotation marks:
+
+            #Albert Einstein once said, “A person who never made a mistake never tried anything new."
+
+print('''
+
+    Madeline Miller once wrote, 
+
+    "He is a weapon, Do not forget it.
+    You can use a spear as a walking stick,
+    but that will not change its nature."
+
+                    - The Song of Achilles
+
+''')
