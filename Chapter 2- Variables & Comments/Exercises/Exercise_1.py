4026ca081c3379e4c9a034b6c5592a720b26b70b
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-07 13:31:23 +0400

    Update Exercise_1.py

--- Chapter 2- Variables & Comments/Exercises/Exercise_1.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise_1.py
@@ -5,4 +5,8 @@
         #Assign a message to a variable, and print that message.
         #Then change the value of the variable to a new message, and print the newmessage.
 
+message = "No man is worth more than another."
+print(message)
 
+message = "Wherever he is from..."
+print(message)
---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_3.py
@@ -0,0 +1,24 @@
+## Exercise 3: Stripping Names 
+
+     #Tidy up the code to make it easier to understand
+        #Use a variable to represent a person’s name, and 
+        #include some whitespace characters at the beginning 
+        #and end of the name. Make sure you use each character
+        #combination, “\t” and “\n”, at least once.
+
+    #Print the name once, so the whitespace around the name is displayed. 
+
+#Variable
+name = "\tLoid Forger\n"
+#Normal
+print("Normal: ")
+print(name)
+#Using lstrip()
+print("\nUsing lstrip():")
+print(name.lstrip())
+#Using rstrip()
+print("\nUsing rstrip():")
+print(name.rstrip())
+#Using strip()
+print("\nUsing strip():")
+print(name.strip())
