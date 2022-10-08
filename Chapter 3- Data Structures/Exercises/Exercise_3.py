d2799b51bbafd2c2148c2c348639069c8360d68e
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-08 15:06:05 +0400

    Create Exercise_4.py

--- Chapter 3- Data Structures/Exercises/Exercise_3.py
+++ Chapter 3- Data Structures/Exercises/Exercise_3.py
@@ -8,8 +8,12 @@
         #these items, such as “I would like toown a Honda motorcycle.”
 
 ## List of Transportation
-fav_transpo = ['car','motorcycle','bus','airpplane','taxi']
+fav_transpo = ['car','motorcycle','bus','airplane','taxi']
 
 # Print 
 print("\nWhat is your favorite mode of transportation?\n")
-print("\t\"I love driving my {0}, it makes me feel safe.\"".format(fav_transpo[0]))
+print("\t\"I love driving my {0}, it makes me feel safe.\"\n".format(fav_transpo[0]))
+print("\nIs there any form of transportation that scares you?\n")
+print("\t\"I think {0}s can be terrifying because its something I cannot control.\"\n".format(fav_transpo[3]))
+print("\nIf you didnt have a {0} which mode of transportation would you use?\n".format(fav_transpo[0]))
+print("\t\"I would probably use the {0} or {1} to go places, its cheaper that way.\"\n".format(fav_transpo[2],fav_transpo[4]))
---
+++ Chapter 3- Data Structures/Exercises/Exercise_4.py
@@ -0,0 +1,58 @@
+## Exercise 4: Guest List
+
+    #If you could invite anyone, living or deceased, to dinner, who
+    #would you invite? Make a list that includes at least three people
+    #you’d like to invite to dinner. Then use your list to print a
+    #message to each person, inviting them to dinner.
+
+## List of Guest
+guests = ['Grell Sutcliff','Ciel Phantomhive','Sebastian Michaelis','Madam Red']
+
+    ## Invitation Print
+# First guest
+name = guests[0].title()
+print('''
+            
+           █▀█ █▀█ █▄█ ▄▀█ █░░  █▀▄ █ █▄░█ █▄░█ █▀▀ █▀█
+           █▀▄ █▄█ ░█░ █▀█ █▄▄  █▄▀ █ █░▀█ █░▀█ ██▄ █▀▄
+
+\"Greetings {0}, you are invited to the royal dinner."\n
+            \"This dinner will be held in the Palace!\"
+
+'''.format(name))
+
+# Second guest
+name = guests[1].title()
+print('''
+            
+           █▀█ █▀█ █▄█ ▄▀█ █░░  █▀▄ █ █▄░█ █▄░█ █▀▀ █▀█
+           █▀▄ █▄█ ░█░ █▀█ █▄▄  █▄▀ █ █░▀█ █░▀█ ██▄ █▀▄
+
+\"Greetings {0}, you are invited to the royal dinner."\n
+            \"This dinner will be held in the Palace!\"
+
+'''.format(name))
+
+# Third guest
+name = guests[2].title()
+print('''
+            
+           █▀█ █▀█ █▄█ ▄▀█ █░░  █▀▄ █ █▄░█ █▄░█ █▀▀ █▀█
+           █▀▄ █▄█ ░█░ █▀█ █▄▄  █▄▀ █ █░▀█ █░▀█ ██▄ █▀▄
+
+\"Greetings {0}, you are invited to the royal dinner."\n
+            \"This dinner will be held in the Palace!\"
+
+'''.format(name))
+
+# Fourth guest
+name = guests[3].title()
+print('''
+            
+           █▀█ █▀█ █▄█ ▄▀█ █░░  █▀▄ █ █▄░█ █▄░█ █▀▀ █▀█
+           █▀▄ █▄█ ░█░ █▀█ █▄▄  █▄▀ █ █░▀█ █░▀█ ██▄ █▀▄
+
+\"Greetings {0}, you are invited to the royal dinner."\n
+            \"This dinner will be held in the Palace!\"
+
+'''.format(name))
