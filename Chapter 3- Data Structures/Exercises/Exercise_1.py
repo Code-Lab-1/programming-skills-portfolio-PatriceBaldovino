2680654b75ec11eb493cc73f86c2d8d84caa21fc
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-08 14:21:07 +0400

    Update Exercise_1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise_1.py
@@ -0,0 +1,16 @@
+## Exercise 1: Names
+
+    #Store the names of a few of your friends in a list called names.
+    #Print each person’s name by accessing each element in the list, one
+    #at a time.
+
+## List of names
+names = ['\tVi','\tCaitlyn','\tMel','\tViktor','\tJinx\n']
+
+#Print of the list
+print("\nSome charcters from the show Arcane:\n")
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
