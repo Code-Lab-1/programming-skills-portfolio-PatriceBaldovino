0cb49c2f1a0498d6b84f707be442b7719f035393
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-11 10:30:15 +0400

    Update Exercise_2.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_2.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_2.py
@@ -1,3 +1,10 @@
 ## Exercise 2: Print the Version of Python 
 
     #Write a Python program to get the Python version you are using.
+
+#Print information
+import sys
+print("Python version\n")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
