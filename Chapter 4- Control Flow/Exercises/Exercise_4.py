64e3955877837defcd0f3598b4a31bac0f141a13
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-11 20:31:08 +0400

    Create Exercise_4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise_4.py
@@ -0,0 +1,68 @@
+## Exercise 4: Stages of Life
+
+    #Write an if-elif-else chain that determines a person’s stage of
+    #life. Set a value for the variable age, and then:
+
+    #•If the person is less than 2 years old, print a message that the person is a baby.
+
+    #•If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+
+    #•If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+
+    #•If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+
+    #•If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+
+    #•If the person is age 65 or older, print a message that the person is an elder.
+
+#Variable
+age = 12
+
+#if-elif-else statement
+if age < 2:
+    print('''
+
+     You're a baby!     lllllll_______
+                     _,_  llllllllllll\\
+        ___         /   \  llllllllllll\\
+           \       | @ @ |  llllllllllll\\
+            \-----oOO---Ooo-------------/
+             \lllllllllllllllllllllllll/
+              \lllllllllllllllllllllll/
+               \lllllllllllllllllllll/
+                   //            \\
+                  ||             ||
+                   OO             OO
+    ''')
+elif age < 4:
+    print('''
+
+   ,=""=,
+  c , _,{   You're a toddler!
+  /\  @ )                 __
+ /  ^~~^\          <=.,__/ '}=
+(_/ ,, ,,)          \_ _>_/~
+ ~\_(/-\)'-,_,_,_,-'(_)-(_)    
+
+    ''')
+elif age < 13:
+    print('''
+        _         _
+       / )%.===.%( \\
+       | // ,,, \\\\ |
+       \/ \/o.o\/ \/    You're a teenager!
+       /\ (  _  ) /\\
+       ^^ /`"""`\ ^^
+         ( (_@_) )
+         /'-.Y.-'\\
+        /    |    \\
+       /___________\\
+          _|_|_|_
+         (___|___)
+         ''')
+elif age < 20:
+    print("You're a teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
