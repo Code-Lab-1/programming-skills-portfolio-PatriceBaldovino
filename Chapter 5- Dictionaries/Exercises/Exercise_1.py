f237435dce40253c32272a3d54988722ac85a26b
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-15 15:31:39 +0400

    Update Exercise_1.py

--- Chapter 5- Dictionaries/Exercises/Exercise_1.py
+++ Chapter 5- Dictionaries/Exercises/Exercise_1.py
@@ -4,4 +4,15 @@
     #last name, age, and the city in which they live. You should have keys such as first_name, 
     #last_name, age, and city. Print each piece of information stored in your dictionary.
 
-    
+#Dictionary
+person = {
+    'first_name': 'Chuuya ',
+    'last_name': 'Nakahara',
+    'age': 22,
+    'city': 'Kanagawa',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
