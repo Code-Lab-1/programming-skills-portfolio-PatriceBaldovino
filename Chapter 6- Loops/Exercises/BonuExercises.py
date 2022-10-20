4c701f101fca9e233651c549c0c35a95a96adfa7
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-20 13:40:43 +0400

    Create BonuExercises.py

---
+++ Chapter 6- Loops/Exercises/BonuExercises.py
@@ -0,0 +1,28 @@
+#Turtle Loop
+
+import turtle
+#circle = 36
+#radius = 120
+#angle = 100
+#for x in range (circle):
+  #turtle.circle(radius)
+  #turtle.left(angle)
+
+#for x in range(6):
+    #turtle.forward(90)
+    #turtle.left(300)
+
+# Starting a Working Screen
+ws = turtle.Screen()
+ 
+# initializing a turtle instance
+geekyTurtle = turtle.Turtle()
+ 
+# executing loop 5 times for a star
+for i in range(5):
+ 
+        # moving turtle 100 units forward
+        geekyTurtle.forward(100)
+ 
+        # rotating turtle 144 degree right
+        geekyTurtle.right(144)
