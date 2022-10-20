085de50337d19b9c09661d1cb3e05d660e483a3b
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-20 13:43:06 +0400

    Update BonuExercises.py

--- Chapter 6- Loops/Exercises/BonuExercises.py
+++ Chapter 6- Loops/Exercises/BonuExercises.py
@@ -14,15 +14,7 @@ import turtle
 
 # Starting a Working Screen
 ws = turtle.Screen()
- 
-# initializing a turtle instance
 geekyTurtle = turtle.Turtle()
- 
-# executing loop 5 times for a star
 for i in range(5):
- 
-        # moving turtle 100 units forward
         geekyTurtle.forward(100)
- 
-        # rotating turtle 144 degree right
         geekyTurtle.right(144)
