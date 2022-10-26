f3b3992dc2b8c46a36cedca68b0f75a6fa40ec04
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-26 12:18:30 +0400

    Create BonusExercise2.py

---
+++ Chapter 6- Loops/Exercises/BonusExercise2.py
@@ -0,0 +1,35 @@
+#print the following - 26/10/2022
+
+print("The following lines will print each letters of Apple")
+apple = ['A','p','p','l','e\n']
+for i in apple:
+  print(i)
+  if i == 'e':
+    break
+
+#print the following
+
+print("The following lines will print each letters of Banana")
+apple = ['B','a','n','a','n','a\n']
+for i in apple:
+  print(i)
+  if i == apple[0::5]:
+    break
+
+#print the following 
+
+print("The following lines will print each letters of Car")
+apple = ['C','a','r\n']
+for i in apple:
+  print(i)
+  if i == 'r':
+    break
+
+#print the following 
+
+print("The following lines will print each letters of Dolphin")
+apple = ['D','o','l','p','h','i','n\n']
+for i in apple:
+  print(i)
+  if i == 'n':
+    break
