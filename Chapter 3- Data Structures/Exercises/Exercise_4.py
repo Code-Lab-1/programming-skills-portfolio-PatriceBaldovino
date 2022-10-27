48824fbacbed3a6a1eb95e297d5fc18b6ac1d394
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:23:15 +0400

    Update Exercise_4.py

--- Chapter 3- Data Structures/Exercises/Exercise_4.py
+++ Chapter 3- Data Structures/Exercises/Exercise_4.py
@@ -1,9 +1,10 @@
 ## Exercise 4: Guest List
 
-    #If you could invite anyone, living or deceased, to dinner, who
-    #would you invite? Make a list that includes at least three people
-    #you’d like to invite to dinner. Then use your list to print a
-    #message to each person, inviting them to dinner.
+'''
+    If you could invite anyone, living or deceased, to dinner, who would you invite? 
+    Make a list that includes at least three people you’d like to invite to dinner. 
+    Then use your list to print a message to each person, inviting them to dinner.
+'''
 
 ## List of Guest
 guests = ['Grell Sutcliff','Ciel Phantomhive','Sebastian Michaelis','Madam Red']
