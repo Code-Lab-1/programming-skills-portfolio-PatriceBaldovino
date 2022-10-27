87d1f3519bd91f25452f60c59c7bf046b603515f
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:19:43 +0400

    Update Exercise_1.py

--- Chapter 2- Variables & Comments/Exercises/Exercise_1.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise_1.py
@@ -2,8 +2,10 @@
 
     #Type out the code below and add the correct data types to complete the variables
 
-        #Assign a message to a variable, and print that message.
-        #Then change the value of the variable to a new message, and print the newmessage.
+'''
+Assign a message to a variable, and print that message.
+ Then change the value of the variable to a new message, and print the newmessage.
+'''
 
 message = "No man is worth more than another."
 print(message)
