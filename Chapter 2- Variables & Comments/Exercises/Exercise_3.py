13e7c7f4b50e02c1d7e0b45fb3a7301bb9e890b7
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:19:45 +0400

    Update Exercise_3.py

--- Chapter 2- Variables & Comments/Exercises/Exercise_3.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise_3.py
@@ -1,10 +1,12 @@
 ## Exercise 3: Stripping Names 
 
-     #Tidy up the code to make it easier to understand
-        #Use a variable to represent a person’s name, and 
-        #include some whitespace characters at the beginning 
-        #and end of the name. Make sure you use each character
-        #combination, “\t” and “\n”, at least once.
+'''
+    Tidy up the code to make it easier to understand
+    Use a variable to represent a person’s name, and 
+    include some whitespace characters at the beginning 
+    and end of the name. Make sure you use each character
+    combination, “\t” and “\n”, at least once.
+'''
 
     #Print the name once, so the whitespace around the name is displayed. 
 
