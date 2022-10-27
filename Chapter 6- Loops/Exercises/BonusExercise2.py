e9402fdce7bd188d78ba16b2641939d8a23167bf
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:16:00 +0400

    Update BonusExercise2.py

--- Chapter 6- Loops/Exercises/BonusExercise2.py
+++ Chapter 6- Loops/Exercises/BonusExercise2.py
@@ -1,38 +1,39 @@
 #print the following - 26/10/2022
 
-#print("The following lines will print each letters of Apple")
-#apple = ['A','p','p','l','e\n']
-#for i in apple:
-#  print(i)
-#  if i == 'e':
-#    break
+'''
+print("The following lines will print each letters of Apple")
+apple = ['A','p','p','l','e\n']
+for i in apple:
+  print(i)
+  if i == 'e':
+    break
 
 #print the following
-
-#print("The following lines will print each letters of Banana")
-#apple = ['B','a','n','a','n','a\n']
-#for i in apple:
-#  print(i)
-#  if i == apple[0::5]:
-#    break
-
-#print the following 
-
-#print("The following lines will print each letters of Car")
-#apple = ['C','a','r\n']
-#for i in apple:
-#  print(i)
-#  if i == 'r':
-#    break
+print("The following lines will print each letters of Banana")
+apple = ['B','a','n','a','n','a\n']
+for i in apple:
+  print(i)
+  if i == apple[0::5]:
+    break
 
 #print the following 
+print("The following lines will print each letters of Car")
+apple = ['C','a','r\n']
+for i in apple:
+  print(i)
+  if i == 'r':
+    break
+'''
 
-#print("The following lines will print each letters of Dolphin")
-#apple = ['D','o','l','p','h','i','n\n']
-#for i in apple:
-#  print(i)
-#  if i == 'n':
-#    break
+'''
+#the following 
+print("The following lines will print each letters of Dolphin")
+apple = ['D','o','l','p','h','i','n\n']
+for i in apple:
+  print(i)
+  if i == 'n':
+    break
+'''
 
 words =["Apple\n","Banana\n","Car\n","Dolphin\n"]
 for word in words:
