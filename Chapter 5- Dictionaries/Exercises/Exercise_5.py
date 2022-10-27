0f2ee21d5b46d79824d0a77ed80d80e5bc8adabe
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:36:06 +0400

    Update Exercise_5.py

--- Chapter 5- Dictionaries/Exercises/Exercise_5.py
+++ Chapter 5- Dictionaries/Exercises/Exercise_5.py
@@ -1,9 +1,10 @@
 ## Exercise 5: Pets
 
-    #Make several dictionaries, where each dictionary represents a different 
-    #pet. In each dictionary, include the kind of animal and the owner’s name. 
-    #Store these dictionaries in a list called pets. Next, loop through your 
-    #list and as you do, print everything you know about each pet
+'''
+    Make several dictionaries, where each dictionary represents a different pet. In each dictionary, 
+    include the kind of animal and the owner’s name. Store these dictionaries in a list called pets.
+    Next, loop through your list and as you do, print everything you know about each pet.
+'''
 
 #list to store the pets in.
 pets = []
