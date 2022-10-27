529a750b509ce00548dc0aff6f0ec74fabd4e1b9
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:19:49 +0400

    Update Exercise_5.py

--- Chapter 2- Variables & Comments/Exercises/Exercise_5.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise_5.py
@@ -1,15 +1,16 @@
 ## Exercise 5: USB Shopper
 
-    #A girl heads to a computer shop to buy some USB 
-    #sticks. She loves USB sticks and wants as many as 
-    #she can get for £50. They are £6 each.
+'''
+    A girl heads to a computer shop to buy some USB 
+    sticks. She loves USB sticks and wants as many as 
+    she can get for £50. They are £6 each.
 
-    #Write a programme that calculates how many USB
-    #sticks she can buy and how many pounds she will
-    #have left.
+    Write a programme that calculates how many USB
+    sticks she can buy and how many pounds she will
+    have left.
+'''
 
-        #You will to use the arithmetic operators to complete 
-        #this exercise.
+    #You will to use the arithmetic operators to complete this exercise.
 
 #Computer Shop
 print('''
