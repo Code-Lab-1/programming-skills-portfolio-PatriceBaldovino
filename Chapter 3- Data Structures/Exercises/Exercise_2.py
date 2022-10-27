738e135a102e9357475a9dadbff0d318edfe29e6
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:23:11 +0400

    Update Exercise_2.py

--- Chapter 3- Data Structures/Exercises/Exercise_2.py
+++ Chapter 3- Data Structures/Exercises/Exercise_2.py
@@ -1,9 +1,11 @@
 ## Exercise 2: Greetings
 
-    #Start with the list you used in Exercise 1, but instead of just
-    #printing each person’s name, print a message to them. The text of
-    #each message should be the same, but each message should be
-    #personalized with the person’s name.
+'''
+    Start with the list you used in Exercise 1, but instead of just
+    printing each person’s name, print a message to them. The text of
+    each message should be the same, but each message should be
+    personalized with the person’s name.
+'''
 
 ## List of names
 names = ['\tVi','\tCaitlyn','\tMel','\tViktor','\tJinx']
