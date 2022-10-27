4c55c601a2224e4938e1e4c2a3f62e171a0f5972
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-27 13:15:58 +0400

    Update BonuExercises.py

--- Chapter 6- Loops/Exercises/BonuExercises.py
+++ Chapter 6- Loops/Exercises/BonuExercises.py
@@ -1,24 +1,24 @@
 #Turtle Loop
-
+'''
 import turtle
-#circle = 36
-#radius = 120
-#angle = 100
-#for x in range (circle):
+circle = 36
+radius = 120
+angle = 100
+for x in range (circle):
   #turtle.circle(radius)
   #turtle.left(angle)
 
-#for x in range(6):
+for x in range(6):
     #turtle.forward(90)
     #turtle.left(300)
 
-# Starting a Working Screen
-#ws = turtle.Screen()
-#geekyTurtle = turtle.Turtle()
-#for i in range(5):
+ Starting a Working Screen
+ws = turtle.Screen()
+geekyTurtle = turtle.Turtle()
+for i in range(5):
         #geekyTurtle.forward(100)
         #geekyTurtle.right(144)
-
+'''
 ## Draw Ears
 import turtle
 pen = turtle.Turtle()
