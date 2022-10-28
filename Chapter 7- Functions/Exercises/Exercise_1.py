554c9b3fb4a9d81164163502f3e62fc8eb4fa06f
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-28 13:34:45 +0400

    Create Exercise_1.py

---
+++ Chapter 7- Functions/Exercises/Exercise_1.py
@@ -0,0 +1,8 @@
+##Exercise 1: Message
+
+'''
+Write a function called display_message() that prints one sentence telling everyone what 
+you are learning about in this chapter. Call thefunction, and make sure the message displays 
+correctly.
+'''
+
