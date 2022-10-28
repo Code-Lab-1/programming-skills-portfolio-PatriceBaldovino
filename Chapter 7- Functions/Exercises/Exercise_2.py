979a3fa6a19a1a445e1f60efd72d112f0924a3c0
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-28 14:36:16 +0400

    Create Exercise_2.py

---
+++ Chapter 7- Functions/Exercises/Exercise_2.py
@@ -0,0 +1,17 @@
+##Exercise 2: Favorite Book
+
+'''
+Write a function called favorite_book() that accepts one parameter, title. 
+The function should print a message, such as One of my favorite books is 
+Alice in Wonderland. Call the function, making sure to include a book title 
+as an argument in the function call.
+'''
+def favorite_book(title):
+    print('''
+    _    
+   /.)   
+  /)\| '''+ title + " is one of my favorite book")
+    print(''' //)/
+/'\"^\"\n''')
+
+favorite_book("The Six of Crows")
