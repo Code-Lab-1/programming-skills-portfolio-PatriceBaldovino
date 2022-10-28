62cb099a355068e350c933c2cb1806f3ba012015
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-10-28 14:24:53 +0400

    Update Exercise_1.py

--- Chapter 7- Functions/Exercises/Exercise_1.py
+++ Chapter 7- Functions/Exercises/Exercise_1.py
@@ -1,8 +1,13 @@
 ##Exercise 1: Message
 
 '''
-Write a function called display_message() that prints one sentence telling everyone what 
-you are learning about in this chapter. Call thefunction, and make sure the message displays 
-correctly.
+Write a function called display_message() that prints one sentence telling everyone 
+what you are learning about in this chapter. Call the function, and make sure the
+message displays correctly.
 '''
+#Function
+def display_message():
+    message = "\nI am learning how to make a function and how to section of the code that performs a specific task."
+    print(message)
 
+display_message()
