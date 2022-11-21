2fd9ffde055479bdab4493010633c7b10f8475d7
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-21 20:30:29 +0400

    Update Extra_Exercise_8.py

---
+++ Chapter 5- Dictionaries/Exercises/Extra_Exercise_8.py
@@ -0,0 +1,22 @@
+#Create a dictionary and do the following:
+
+'''
+    - Print the dictionary, its keys and items
+'''
+print()
+
+student = {
+    "Name" : "Nagisa", "Age" : "17", "Year" : "Year 1", "Gender" : "Male", "Location" : "Japan"
+}
+
+print(student)
+
+print()
+
+print(student.keys())
+
+print()
+
+print(student.items())
+
+print()
