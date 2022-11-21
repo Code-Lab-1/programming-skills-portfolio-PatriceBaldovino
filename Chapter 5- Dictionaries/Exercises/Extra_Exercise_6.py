5e8462977805b0db25c0d54668e264875f1ffbac
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-21 20:08:56 +0400

    Create Extra_Exercise_6.py

---
+++ Chapter 5- Dictionaries/Exercises/Extra_Exercise_6.py
@@ -0,0 +1,8 @@
+#Create a dictionary with the information of the Student
+
+#Dictionary
+student = {
+    "Name" : "\nNagisa\n", "Age" : "\n17\n", "Year" : "\nYear 1\n", "Gender" : "\nMale\n", "Location" : "\nJapan\n"
+}
+
+print(student["Name"])
