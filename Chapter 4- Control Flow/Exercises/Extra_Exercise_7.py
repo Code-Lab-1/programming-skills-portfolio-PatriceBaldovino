d04f2c9c25db5bc3b19618073c55b6d01e7cb54d
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-21 19:36:08 +0400

    Create Extra_Exercise_7.py

---
+++ Chapter 4- Control Flow/Exercises/Extra_Exercise_7.py
@@ -0,0 +1,9 @@
+'''Write a program which asks the user for a number. 
+    If number is even print ‘Even’, else print ‘Odd’.'''
+
+num = int(input("Enter number: "))
+
+if num % 2 == 0:
+    print("It's Even")
+else:
+    print("It's Odd")
