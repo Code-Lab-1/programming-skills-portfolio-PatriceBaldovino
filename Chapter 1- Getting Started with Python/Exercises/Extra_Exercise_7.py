c83aea806548a3313b4db6c85c327f563f1848a4
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:02:15 +0400

    Update Extra_Exercise_7.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Extra_Exercise_7.py
@@ -0,0 +1,18 @@
+#Print the subtraction of two numbers
+    #September 15, 2022
+
+print()
+print("#Print the subtraction of two numbers")
+print()
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# minus two numbers
+difference = int(num1) - int(num2)
+ 
+# Display the difference
+print()
+print('The difference of {0} and {1} is {2}'.format(num1, num2, difference))
+print()
