985e5400a460fa343a90d9ca5af9b71201707c03
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:31:49 +0400

    Update Extra_Exercise_9.py

---
+++ Chapter 2- Variables & Comments/Exercises/Extra_Exercise_9.py
@@ -0,0 +1,12 @@
+#Online variable exercise
+
+'''
+Below is a good example of mixing numbers and text inside the print() function
+Assign: 3 to variable glass_of_water.
+'''
+
+#Type here. Assign a number to the variable: glass_of_water
+
+glass_of_water = 3
+
+print("I drank", glass_of_water, "glasses of water today.")
