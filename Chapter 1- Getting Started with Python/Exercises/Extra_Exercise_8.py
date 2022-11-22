568078037e3f631444df5887416e701e14440b24
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:02:17 +0400

    Update Extra_Exercise_8.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Extra_Exercise_8.py
@@ -0,0 +1,18 @@
+#Print the multiplication of 2 numbers
+    #September 15, 2022
+
+print()
+print("#Print the multiplication of 2 numbers")
+print()
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# multiply two numbers
+product = int(num1) * int(num2)
+ 
+# Display the product
+print()
+print('The product of {0} and {1} is {2}'.format(num1, num2, product))
+print()
