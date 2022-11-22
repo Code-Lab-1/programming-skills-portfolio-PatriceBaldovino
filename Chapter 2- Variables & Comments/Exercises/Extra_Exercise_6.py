fbe1775bf3185d43c72dcf35f09dd2e589731076
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:31:43 +0400

    Update Extra_Exercise_6.py

---
+++ Chapter 2- Variables & Comments/Exercises/Extra_Exercise_6.py
@@ -0,0 +1,12 @@
+#Setember 21, 2022
+
+#Variables
+name = "Patrice Baldovino"
+age = 17
+contact_number = "564308929"
+email = "arrebo@gmail.com"
+department = "Creative Computing"
+level = "Year 1"
+
+#Printing Valuables
+print (name, age, contact_number, email, department, level)
