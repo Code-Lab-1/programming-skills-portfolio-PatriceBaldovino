2afad7284821fdc3d5313b2597a2b3d0f0423557
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:44:19 +0400

    Update Exercise_3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_3.py
@@ -1,9 +1,11 @@
-## Exercise 3: Print date and Time :ballot_box_with_check:
+## Exercise 3: Print date and Time 
 
     #Write a Python program to display the current date and time.
 
 #Print the time
 import datetime
 now = datetime.datetime.now()
+print()
 print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
+print()
