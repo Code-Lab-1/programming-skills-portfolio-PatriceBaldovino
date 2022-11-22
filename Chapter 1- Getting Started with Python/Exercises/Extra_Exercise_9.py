f39210ee33b1a671b7247b94dffa1f091753bc0f
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:02:19 +0400

    Update Extra_Exercise_9.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Extra_Exercise_9.py
@@ -0,0 +1,18 @@
+#Divide the number 5 by 2 and print the answer
+    #September 15, 2022
+
+print()
+print("#Divide the number 5 by 2 and print the answer")
+print()
+
+# Store input numbers
+number1= 5
+number2= 2
+
+# divide two numbers
+quotient = float(number1/number2)
+ 
+# Display the quotient
+print()
+print('The quotient of {0} and {1} is {2}'.format(number1, number2, quotient))
+print()
