3cd7dea5ce567a686c77ebd4f1f7078a843d194a
PatriceBaldovino <114474524+PatriceBaldovino@users.noreply.github.com>
2022-11-22 11:31:40 +0400

    Update Extra_Exercise_10.py

---
+++ Chapter 2- Variables & Comments/Exercises/Extra_Exercise_10.py
@@ -0,0 +1,19 @@
+#Online variable exercise
+
+'''
+Let's try to see what happens after assigning a new 
+value to our variable. Note that program gets executed line by line.
+
+Place the variable: glass_of_water inside the print 
+function and observe what happens.
+'''
+
+#Fill the print function so it prints glass_of_water
+
+glass_of_water=3
+
+glass_of_water=glass_of_water + 1
+
+print()
+print("You have to drink {0} glasses of water a day".format (glass_of_water))
+print()
